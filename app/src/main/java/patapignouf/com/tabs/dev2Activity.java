7285e7a3fa67b291a781571b284f18b9f914cdcb
Patapignouf <quentinkhoanguyen@gmail.com>
2017-11-20 22:55:05 +0100

    Corrections

--- app/src/main/java/patapignouf/com/tabs/dev2Activity.java
+++ app/src/main/java/patapignouf/com/tabs/dev2Activity.java
@@ -16,7 +16,10 @@ public class dev2Activity extends AppCompatActivity {
         String tdev2 = getResources().getString(R.string.textdev2_1);
         String tdev2_2 = getResources().getString(R.string.textdev2_2);
         String tdev2_3 = getResources().getString(R.string.textdev2_3);
-
+        String tdev2_4 = getResources().getString(R.string.textdev2_4);
+        String tdev2_5 = getResources().getString(R.string.textdev2_5);
+        String tdev2_6 = getResources().getString(R.string.textdev2_6);
+        String tdev2_7 = getResources().getString(R.string.textdev2_7);
 
         final TextView tVdev2;
         tVdev2 = (TextView) findViewById(R.id.textViewDev2_1);
@@ -29,6 +32,23 @@ public class dev2Activity extends AppCompatActivity {
         final TextView tVdev2_3;
         tVdev2_3 = (TextView) findViewById(R.id.textViewDev2_3);
         tVdev2_3.setText(tdev2_3);
+
+        final TextView tVdev2_4;
+        tVdev2_4 = (TextView) findViewById(R.id.textViewDev2_4);
+        tVdev2_4.setText(tdev2_4);
+
+        final TextView tVdev2_5;
+        tVdev2_5 = (TextView) findViewById(R.id.textViewDev2_5);
+        tVdev2_5.setText(tdev2_5);
+
+        final TextView tVdev2_6;
+        tVdev2_6 = (TextView) findViewById(R.id.textViewDev2_6);
+        tVdev2_6.setText(tdev2_6);
+
+        final TextView tVdev2_7;
+        tVdev2_7 = (TextView) findViewById(R.id.textViewDev2_7);
+        tVdev2_7.setText(tdev2_7);
+
 
     }
 }
